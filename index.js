09614a86497a74310c25706554de50a9fd6c657f
Wendy <wendynjuguna714@gmail.com>
2022-11-23 15:13:01 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Wendy Njuguna";
+const welcomeMessage = "Welcome to Flatbook, Wendy Njuguna,!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, WENDY NJUGUNA,!";
+const shortGreeting = "Welcome, W !";
